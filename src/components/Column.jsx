5affde857b3b0f2e42a6c31477a101e695b37b7a
Pader <paderx26@gmail.com>
2026-01-31 07:08:17 -0600

    added zustand store

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,9 +1,22 @@
 import "./Column.css"
 import Task from "./Task"
+import { useStore } from "../store"
 
 export default function Column({ state }) {
-return <div className="column">
+const tasks = useStore((store)=> 
+    store.tasks.filter((task)=>task.state === state)
+);
+// console.log(tasks, "working?")
+
+return 
+<div className="column">
+    <div className="titleWrapper">
     <p>{state}</p>
-    <Task title="To Do" />
+    <button>Add</button>
+    </div>
+    
+    {tasks.map((task)=>
+        (<Task title={task.tile} key={task.title}/>
+        ))}
 </div>
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,16 @@
+import classNames from 'classnames';
+import { useStore } from '../store'
 import './Task.css'
 
-let STATUS ="PLANNED";
+export default function Task({ title }) {
 
-export default function Task({title}) {
+    const task = useStore((store) =>store.tasks.find(task=>task.title === title))
     return(
         <div className="task">
-            <div>{title}</div>
+            <div>{task.title}</div>
                 <div className='bottomWrapper'>
                     <div></div>
-                    <div className='status'>{STATUS}</div>
+                    <div className={classNames("status", task.state)}>{task.state}</div>
                 </div>
         </div>
     );
