3248285e77f826b289ab368ca54b856a8cbd6cc8
Pader <paderx26@gmail.com>
2026-02-05 13:16:46 -0600

    more column coding and store info

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -3,20 +3,26 @@ import Task from "./Task"
 import { useStore } from "../store"
 
 export default function Column({ state }) {
+const [text, setText] = useState('');
+const [open, setOpen] = useState(false)   
+    
 const tasks = useStore((store)=> 
     store.tasks.filter((task)=>task.state === state)
 );
 // console.log(tasks, "working?")
 
+const addTask = useStore(store=>store.addTask)
+
 return 
 <div className="column">
     <div className="titleWrapper">
     <p>{state}</p>
-    <button>Add</button>
+    <button 
+    onClick={() => setOpen(true)}>Add</button>
     </div>
     
     {tasks.map((task)=>
-        (<Task title={task.tile} key={task.title}/>
+        (<Task title={task.title} key={task.title}/>
         ))}
-</div>
+</div>    
 }
--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { create } from 'zustand'
  
-const store = (set) => {{
-    tasks: [{title: "TestTask", state: "PLANNED"}]
-}};
+const store = (set) => ({
+    tasks: [{title: "TestTask", state: "ONGOING"}],
+    addTask: (title, state) => 
+        set((store) => ({ tasks: [...store,tasks, { title, state}] })),
+});
 
-export const useStore = create(store)
+export const useStore = create(store) 
