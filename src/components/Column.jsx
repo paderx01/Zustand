6dfa7fc952954809d1af76340fa23755e2c2d8ee
Pader <paderx26@gmail.com>
2026-02-05 13:27:04 -0600

    column modal and modal content. also updated modal css

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,6 +1,7 @@
-import "./Column.css"
-import Task from "./Task"
-import { useStore } from "../store"
+import "./Column.css";
+import Task from "./Task";
+import { useStore } from "../store";
+import {useState} from 'react';
 
 export default function Column({ state }) {
 const [text, setText] = useState('');
@@ -24,5 +25,19 @@ return
     {tasks.map((task)=>
         (<Task title={task.title} key={task.title}/>
         ))}
+        {open && (
+        <div className='Modal'>
+            <div className='modalContent'>
+                <input onChange={(e)=>setText(e.target.value)} value={text} />
+                <button onClick={()=>{
+                    addTask(text, state);
+                    setText('');
+                    setOpen(false);
+                }}>
+                    Submit
+                    </button>
+            </div>
+        </div>
+        )};
 </div>    
 }
