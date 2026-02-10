3efa32eb6e229fc60c801eec82156cfe57814f61
Pader <paderx26@gmail.com>
2026-02-09 19:56:28 -0600

    test store = logical problems that spread throughout all of state logic in all parts of the app

--- src/store.test.jsx
+++ src/store.test.jsx
@@ -1,4 +1,9 @@
 import { useStore } from './store'
+import { render } from '@testing-librart/react';
+import { vi } from 'vitest';
+import { useStore } from './store';
+
+vi.mock('zustand')
 
 function TestComponent({selector, effect}){
     const items = useStore(selector)
@@ -9,7 +14,34 @@ useEffect(()=> effect(items), [items]);
 };
   
 
-test('should return default value at the start', ()=>{
-    const selector = (store) =>store.tasks;
-    const effect = vi.fn()
+test('should add an items to the store and rerun the effect', ()=>{
+    const selector = (store) =>({tasks: store.tasks, addTask: store.addTask });
+    const effect = vi.fn().mockImplementation((items)=>{
+        if(items.tasks.length === 0) {
+            items.addTask('a', 'b')
+        }
+    });
+    render(<TestComponent selector={selector} effect={effect}/>);
+    expect(effect).toHaveBeenCalledTimes(2);
+    expect(effect).toHaveBeenCalledWith(expect.objectContaining({tasks: [{
+        title: 'a', state: 'b'
+    }]}));
+});
+
+test('should add an items to the store and rerun the effect', ()=>{
+    const selector = (store) =>({tasks: store.tasks, addTask: store.addTask, deleteTask });
+    let createdTask = false;
+    let currentItems;
+    const effect = vi.fn().mockImplementation((items)=>{
+        currentItems = items;
+        if(!createdTask) {
+            items.addTask('a', 'b')
+            createdTask = true;
+        } else if (items.tasks.length === 1) {
+          items.deleteTask('a');  
+        }
+    });
+    render(<TestComponent selector={selector} effect={effect}/>);
+    expect(effect).toHaveBeenCalledTimes(2);
+    expect(currentItems.tasks).toEqual([])
 });
